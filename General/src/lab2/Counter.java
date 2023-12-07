87fd161fd481909b6d2ebed89f99209ecfb00feb
Bhabishwor <gbhabishwor22@tbc.edu.np>
2023-12-07 18:50:32 +0545

    Modified the code in lab2

--- General/src/lab2/Counter.java
+++ General/src/lab2/Counter.java
@@ -1,8 +1,12 @@
 package lab2;
 
+/**
+ * Defines methods for counting words, letters and the length
+ * of the sentence.
+ */
 public interface Counter {
 	/**
-	 * Counts words in the sentence.
+	 * Counts words in the given sentence.
 	 * 
 	 * @param sentence The given sentence.
 	 * @return The number of words in the sentence.
@@ -10,7 +14,7 @@ public interface Counter {
 	int countWords(String sentence);
 	
 	/**
-	 * Counts letters in the sentence.
+	 * Counts letters in the given sentence.
 	 * 
 	 * @param sentence The given sentence.
 	 * @return The number of letters in the sentence.
@@ -18,7 +22,7 @@ public interface Counter {
 	int countLetters(String sentence);
 	
 	/**
-	 * Gets length of the sentence.
+	 * Gets length of the given sentence.
 	 * 
 	 * @param sentence The given sentence.
 	 * @return The length of the sentence.
--- General/src/lab2/Driver.java
+++ General/src/lab2/Driver.java
@@ -2,26 +2,31 @@ package lab2;
 
 import java.util.Scanner;
 
+/**
+ * Serves as the entry point for the program that counts the
+ * letters, words and length of a sentence.
+ */
 public class Driver {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		WordProcessor processor = new WordProcessor();
 		
-		// Asks for the sentence to be processed.
+		// Asks for the sentence that is to be processed.
 		System.out.println("Enter a sentence.");
 		String sentence = scan.nextLine();
 		
 		// Tests methods for the user input sentence.
-		processor.countLetters(sentence);
-		processor.countWords(sentence);
-		processor.getLength(sentence);
-		
-		// Sets the text that prints when there is no sentence.
-		processor.setText("There is no sentence.");
-		
+		System.out.println("\nData for user's sentence.");
+		System.out.println("Letters: " + processor.countLetters(sentence));
+		System.out.println("Words: " + processor.countWords(sentence));
+		System.out.println("Length: " + processor.getLength(sentence));
+
 		// Tests methods for the null.
-		processor.countLetters(null);
-		processor.countWords(null);
-		processor.getLength(null);
+		System.out.println("\nData for null sentence.");
+		System.out.println("Letters: " + processor.countLetters(null));
+		System.out.println("Words: " + processor.countWords(null));
+		System.out.println("Length: " + processor.getLength(null));
+		
+		scan.close();
 	}
 }
