46c6a8ec94c455fa927b34efb605d85a2ed01c41
Bhabishwor <gbhabishwor22@tbc.edu.np>
2023-12-08 18:10:45 +0545

    Updated code of lab5

--- General/src/lab5/Driver.java
+++ General/src/lab5/Driver.java
@@ -1,5 +1,9 @@
 package lab5;
 
+/**
+ * Serves as the entry point for the program that checks the 
+ * functionality of methods of NameManager and NumberSorter class.
+ */
 public class Driver {
 	public static void main(String[] args) {
 		NameManager manager = new NameManager();
@@ -9,8 +13,17 @@ public class Driver {
 		manager.addName("P.Smith");
 		manager.addName("Peter Jonathan Smythton");
 		manager.addName("P.Thompson");
+		
 		manager.printNames(); // should print all names
+		
 		manager.removeLongNames();
+		
 		manager.printNames(); // should now have longer names removed	
+		
+		NumberSorter sorter = new NumberSorter();
+
+		sorter.sort(new int [] {1,9,2,3,10,8,12,1, 99, 2, 43, 68, 109, 0}, true);
+		sorter.sort(new int [] {1,2,3,4}, false);
+		sorter.sort(new int [] {4,3,2,1}, true);
 	}
 }
--- General/src/lab5/NumberSorter.java
+++ General/src/lab5/NumberSorter.java
@@ -2,8 +2,22 @@ package lab5;
 
 import java.util.Stack;
 
+/**
+ * Represents a sorting system that sorts out the array of numbers in
+ * ascending or descending order.
+ */
 public class NumberSorter {
-	void sort(int[] numbers) {
+	/**
+	 * Sorts the numbers in numbers array in ascending or descending 
+	 * order
+	 * 
+	 * @param numbers An array of numbers to sort
+	 * @param ascending A boolean flag indicating whether the order 
+	 * should be ascending or descending.
+	 * 			- If {@code true}, ascending sorting is done.
+	 * 			- If {@code false}, descending sorting is done.
+	 */
+	void sort(int[] numbers, boolean ascending) {
 		// The source stack, from which the numbers are popped
 		Stack<Integer> srcStack = new Stack<Integer>();
 		
@@ -14,10 +28,28 @@ public class NumberSorter {
 		for (int number : numbers)
 			srcStack.push(number);
 		
-		while (srcStack.size() > 0) {
-			Integer next = srcStack.pop();
-			
-			while (destStack.size() > 0 && destStack.peek() > next) {}
+		if (ascending == true) {
+			// Adds the array of numbers in destination stack in ascending order.
+			while (srcStack.size() > 0) {
+				Integer next = srcStack.pop();
+				
+				while (destStack.size() > 0 && destStack.peek() > next)
+					srcStack.push(destStack.pop());		
+				destStack.push(next);
+			}
+		} else {
+			// Adds the array of numbers in destination stack in descending order.
+			while (srcStack.size() > 0) {
+				Integer next = srcStack.pop();
+				
+				while (destStack.size() > 0 && destStack.peek() < next)
+					srcStack.push(destStack.pop());		
+				destStack.push(next);
+			}
 		}
+		
+		// Prints the sorted numbers out.
+		for (int number : destStack)
+			System.out.println(number);
 	}
 }
